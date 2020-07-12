56a811a447d20492805774b2b2e81aa002a23a3c
Nguyen Hai-Son <nhson17@apcs.vn>
2020-07-12 13:05:14 +0700

    So far: Sprites + Physics + Animations - Platform game

---
+++ A1 - Design 2D scene/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 5f;
+    public float jumpSpeed= 5f;
+    public float movement= 0f;
+    private Rigidbody2D rigidBody;
+
+    public Transform groundCheckPoint;
+    public float groundCheckRadius;
+    public LayerMask groundLayer;
+    private bool isTouchingGround;
+
+    private Animator playerAnimation;
+    private SpriteRenderer playerSpriteRenderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+        playerAnimation = GetComponent<Animator>();
+        playerSpriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isTouchingGround = Physics2D.OverlapCircle(groundCheckPoint.position, groundCheckRadius, groundLayer);
+        movement = Input.GetAxis("Horizontal");
+        if (movement > 0f )
+        {
+            rigidBody.velocity = new Vector2(movement * speed, rigidBody.velocity.y);
+            playerSpriteRenderer.flipX = false;
+        }
+        else if ( movement < 0f)
+        {
+            rigidBody.velocity = new Vector2(movement * speed, rigidBody.velocity.y);
+            playerSpriteRenderer.flipX = true;
+        }
+        else
+        {
+            rigidBody.velocity = new Vector2(0, rigidBody.velocity.y);
+        }
+
+        if (Input.GetButtonDown("Jump") && isTouchingGround)
+        {
+            rigidBody.velocity = new Vector2(rigidBody.velocity.x, jumpSpeed);
+        }
+
+        playerAnimation.SetFloat("Speed",Math.Abs(rigidBody.velocity.x));
+        playerAnimation.SetBool("OnGround", isTouchingGround);
+    }
+}
