14593ee4c6745572b72c9c7840e4d1ee1c646b44
Nguyen Hai-Son <nhson17@apcs.vn>
2020-07-12 14:26:51 +0700

    Add new animations: CoinSpin + MovingPlank

---
+++ A1 - Design 2D scene/Assets/Scripts/CoinScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinScript : MonoBehaviour
+{
+    public int score = 0;
+    public int rotateSpeed = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, rotateSpeed, 0, Space.World);
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        Destroy(gameObject);
+        score += 5;
+    }
+}
